753f672edecf279b0a94575914f50cf7fdc74325
Nguyễn Trúc <xuantrucx111@gmail.com>
2023-06-16 14:30:56 +0700

    create login screen

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 1337
 
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'home_page.html'));
+  res.sendFile(path.join(__dirname, '/home_page.html'));
 });
 
 app.listen(port, () => {
