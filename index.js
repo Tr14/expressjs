61fc6027fe76891e7475efc569a6d6ef9789e082
Nguyễn Trúc <xuantrucx111@gmail.com>
2023-06-16 14:49:06 +0700

    create login screen

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const path = require('path')
-const app = new express()
+const app = express()
+const router = express.Router();
 const port = 1337
 
-app.use(express.static('public'))
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'))
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/home_page.html'))
 })
 
+router.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname +'/login_page.html'));
+});
+
+app.use('/', router);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
