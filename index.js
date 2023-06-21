e80b17f152bc56db9385b1eefb9a131d4194a123
Tr14 <xuantrucx111@gmail.com>
2023-06-21 19:45:45 +0700

    3rd website change

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', '/index.html'))
 })
 
-app.get('/login', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', '/login.html'))
+app.get('/shop', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', '/shop.html'))
+})
+
+app.get('/detail', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', '/detail.html'))
+})
+
+app.get('/cart', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', '/cart.html'))
+})
+
+app.get('/checkout', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', '/checkout.html'))
+})
+
+app.get('/contact', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', '/contact.html'))
 })
 
 app.listen(port, () => {
