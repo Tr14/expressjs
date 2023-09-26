1e206975eea287430a21aaea8084b4eedeb495cf
Nguyễn Trúc <xuantrucx111@gmail.com>
2023-09-26 10:11:47 +0700

    orc

---
+++ public/webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BLvewSg0a3QdJ2bBjXBPIrE6ptBeOK_RG6TjMD8rIjF231oBNiPZIpVN9_D8LXmWsvb-RZCc1bs0UDQeaI_R9-Q=","apiKey":"ABEQ2lCmCnZ9DrCbxADBZW7U0","accountToken":"ABEhxsBlvacM1-Zr9vn2CflUc","appver":"0.0.0","apiHost":"https://tmq1qj2-webpush.oraclersys.com","lazy":false}';
+orawp.init(config);
